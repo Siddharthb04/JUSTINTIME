2514a5229d77a7cb4652e222944671e3d606d796
Siddharthb04 <137971611+Siddharthb04@users.noreply.github.com>
2025-03-17 14:25:43 +0530

    Add files via upload

---
+++ frontend/src/pages/Orders/Shipping.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  savePaymentMethod,
+} from "../../redux/features/cart/cartSlice";
+import ProgressSteps from "../../components/ProgressSteps";
+
+const Shipping = () => {
+  const cart = useSelector((state) => state.cart);
+
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate("/placeorder");
+  };
+
+ 
+
+  return (
+    <div className="container mx-auto mt-10">
+      <ProgressSteps step1 step2 />
+      <div className="mt-[10rem] flex justify-around items-center flex-wrap">
+        <form onSubmit={submitHandler} className="w-[40rem]">
+          <div className="mb-4">
+            <label className="block text-gray-400">Select Method</label>
+            <div className="mt-2">
+              <label className="inline-flex items-center">
+                <input
+                  type="radio"
+                  className="form-radio text-pink-500"
+                  name="paymentMethod"
+                  value="PayPal"
+                  checked={paymentMethod === "PayPal"}
+                  onChange={(e) => setPaymentMethod(e.target.value)}
+                />
+
+                <span className="ml-2">PayPal or Credit Card</span>
+              </label>
+            </div>
+          </div>
+
+          <button
+            className="bg-pink-500 text-white py-2 px-4 rounded-full text-lg w-full"
+            type="submit"
+          >
+            Continue
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Shipping;
